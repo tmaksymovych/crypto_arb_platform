8770bf1d1d18345391963b4fc8994262e93ca28e
tymur <tmaksymovych@kse.org.ua>
2026-02-13 03:55:51 +0200

    first working features_arb prototype | +description in readme

--- services/analyzer/src/main.py
+++ services/analyzer/src/main.py
@@ -15,51 +15,73 @@ logger = logging.getLogger("Analyzer")
 
 REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
 
-MIN_VOLUME_USDT = 90000.0
-THRESHOLD = 0.75  # Arbitrage threshold in percentage
-SIGNAL_TTL = 30  # Time to live for arbitrage signals in seconds (2.5 minutes)
+LEVERAGE = 6    # Arbitrage threshold in percentage
+THRESHOLD = 0.6   # Minimum spread percentage to consider for arbitrage
+ESTIMATED_FEE_TOTAL = 0.22  # Estimated total fees for arbitrage
+MIN_VOLUME_USDT = 5000000.0 # 5 million USD minimum volume to consider for arbitrage
+SIGNAL_TTL = 5 
 BLACKLIST = ['U/USDT']
 
-async def check_arbitrage(r, symbol, buy_exchange, sell_exchange, buy_ticker, sell_ticker):
+async def check_futures_arbitrage(r, symbol, long_exchange, short_exchange, long_ticker, short_ticker):
     """
     check arbitrage between two exchanges nad control timer
     """
-    buy_price = buy_ticker.ask
-    sell_price = sell_ticker.bid
+    long_price = long_ticker.ask
+    short_price = short_ticker.bid
+
+    if long_price == 0 or short_price == 0:
+        return
     
-    if buy_price == 0 or sell_price == 0:
+   # 1. "Грязный" спред в процентах
+    spread = ((short_price - long_price) / long_price) * 100
+
+    # Если спред меньше порога (0.6%), даже не смотрим, чтобы не кормить биржу
+    if spread < THRESHOLD:
         return
 
-    profit = (sell_price / buy_price - 1) * 100
-    signal_key = f"signal:{symbol}:{buy_exchange}_to_{sell_exchange}"
+    # 2. Расчет чистой прибыли
+    # Сколько мы отдадим бирже за круг (4 сделки)
+    fees_cost = ESTIMATED_FEE_TOTAL # 0.22%
+    
+    # Чистый спред (Spread - Fees)
+    net_spread = spread - fees_cost
+    
+    # 3. ROE (Прибыль на вложенные свои деньги с учетом плеча)
+    # Пример: Спред 1%, Комиссия 0.22%, Чистыми 0.78%. Плечо x6.
+    # ROE = 0.78% * 6 = 4.68%
+    roe = net_spread * LEVERAGE
 
-    if profit >= THRESHOLD and profit <= 50: # if we have profit
-        
-        
+    signal_key = f"futures_signal:{symbol}:{long_exchange}->{short_exchange}"
 
+    if net_spread > 0 and net_spread < 5:
         start_time = await r.get(signal_key)
 
         if not start_time:
-            # it is 1st signal, start timer
-            await r.set(signal_key, time.time())
-            # logger.info(f"New arbitrage signal: {symbol} {buy_exchange}→{sell_exchange} profit={profit:.2f}%")
+            await r.set(signal_key, time.time(), ex=SIGNAL_TTL)
+            # logger.info(f"NEW {symbol} {long_exchange}->{short_exchange} Spread: {spread:.2f}%")    
         else:
-            # signal already exists, check duration
             duration = time.time() - float(start_time)
-
             if duration >= SIGNAL_TTL:
-                logger.info(f"Signal confirmed: {symbol} {buy_exchange}→{sell_exchange} duration={duration/60:.1f}min profit={profit:.2f}%")
-                # here you call telegram bot
-                await r.delete(signal_key)    
-            # else:
-        #     # to not spam logs, we log only once per minute
-        #     if int(duration) % 60 < 5:
-        #         logger.info(f"Signal pending: {symbol} {buy_exchange}→{sell_exchange} duration={duration/60:.1f}min profit={profit:.2f}%")
+                logger.info(f"CONFIRMED {symbol} Profit: {net_spread:.2f}% ROE: {roe:.2f}%")
+                # msg = (
+                #         f"🚀 <b>FUTURES ARB (x{LEVERAGE})</b>\n\n"
+                #         f"💎 <b>{symbol}</b>\n"
+                #         f"📉 <b>SHORT: {short_ex.upper()}</b> ({short_price})\n"
+                #         f"📈 <b>LONG:  {long_ex.upper()}</b> ({long_price})\n\n"     
+                #         f"📊 Raw Spread: {spread:.2f}%\n"
+                #         f"💸 Fees: -{fees_cost}%\n"
+                #         f"🟢 <b>Net Spread: {net_spread:.2f}%</b>\n\n"
+                        
+                #         f"🔥 <b>Est. ROE: {roe:.2f}%</b> (Net Profit)\n"
+                #         f"⏱ Duration: {duration:.1f}s"
+                #     )
+                # await send_telegram_alert(msg)
+                await r.delete(signal_key)
     else:
-        # if profit drops below threshold, remove signal
         if await r.exists(signal_key):
             await r.delete(signal_key)
-            # logger.info(f"Signal removed: {symbol} {buy_exchange}→{sell_exchange} profit={profit:.2f}%")
+
+
 
 
 async def main():
@@ -113,8 +135,8 @@ async def main():
                         if ticker1.quoteVolume < MIN_VOLUME_USDT or ticker2.quoteVolume < MIN_VOLUME_USDT:
                             continue
 
-                        await check_arbitrage(r, symbol, EX1, EX2, ticker1, ticker2)
-                        await check_arbitrage(r, symbol, EX2, EX1, ticker2, ticker1)
+                        await check_futures_arbitrage(r, symbol, EX1, EX2, ticker1, ticker2)
+                        await check_futures_arbitrage(r, symbol, EX2, EX1, ticker2, ticker1)
 
             await asyncio.sleep(2)  # small pause to not overload Redis
 
